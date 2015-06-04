a64e7063d0ce1c48ffc345ebc6e75c3b3dea4995
Eric Katz <ericpkatz@gmail.com>
2015-06-04 01:18:00 +0000

    some refactoring

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -1,14 +1,25 @@
 var FizzBuzz = {
   generate: function(max) {
+    function isFizz(num){
+      return num % 3 == 0;
+    }
+    
+    function isBuzz(num){
+      return num % 5 == 0;
+    }
+    
+    function isFizzBuzz(num){
+      return isFizz(num) && isBuzz(num);
+    }
     var results = [];
     for (var i = 1; i <= max; i = i + 1) {
-      if (i % 3 == 0 && i % 5 == 0) {
+      if (isFizzBuzz(i)) {
         results.push('FizzBuzz');
       }
-      else if (i % 3 == 0) {
+      else if (isFizz(i)) {
         results.push('Fizz');
       }
-      else if (i % 5 == 0) {
+      else if (isBuzz(i)) {
         results.push('Buzz');
       }
       else {
