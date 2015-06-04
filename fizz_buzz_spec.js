a950bff714ee044fbc46932d8a43425227acb193
Eric Katz <ericpkatz@gmail.com>
2015-06-04 01:10:15 +0000

    get FizzBuzz to work

--- fizz_buzz_spec.js
+++ fizz_buzz_spec.js
@@ -1,5 +1,23 @@
+/* globals describe it expect FizzBuzz */
 describe("FizzBuzz", function(){
     it("exists", function(){
         expect(FizzBuzz).toBeDefined();
+    });
+    describe("#generate", function(){
+        it("when passed 1 returns [1]", function(){
+            expect(FizzBuzz.generate(1)).toEqual([1]);
+        });
+        it("when passed 2 returns [1, 2]", function(){
+            expect(FizzBuzz.generate(2)).toEqual([1,2]);
+        });
+        it("when passed 3 it returns [1, 2, 'Fizz']", function(){
+            expect(FizzBuzz.generate(3)).toEqual([1,2,'Fizz']);
+        });
+        it("when passed 5 it returns [1, 2, 'Fizz', 4, 'Buzz']", function(){
+           expect(FizzBuzz.generate(5)).toEqual([1, 2, 'Fizz', 4, 'Buzz']); 
+        });
+        it("when passed 15 last element is 'Fizz Buzz'", function(){
+            expect(FizzBuzz.generate(15)[14]).toEqual('FizzBuzz');
+        });
     });
 });
